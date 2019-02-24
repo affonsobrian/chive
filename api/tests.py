d2a25d290ca8967b6ed9b4987e59081c7ba408bb
Affonso Brian Pereira Azevedo <contato.affonsobrian@gmail.com>
2019-02-24 10:50:41 -0300

    Add simple tests just for purpose of testing the technology

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class TestDjangoTests(TestCase):
+
+    def tests_ten_equals_ten(self):
+        self.assertEqual(10, 10)
+
+    def test_ten_almost_equals_nine(self):
+        self.assertAlmostEqual(10, 9, delta=1)
