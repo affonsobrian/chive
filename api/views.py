6532e1b49cb7ec783608dfe959f01cdb17293883
Affonso Brian Pereira Azevedo <contato.affonsobrian@gmail.com>
2019-02-24 10:03:50 -0300

    Add basic endpoint for purposes of testing

--- api/views.py
+++ api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# ViewSets define the view behavior.
+from django.contrib.auth.models import User
+from rest_framework import viewsets
+from api.serializers import UserSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
